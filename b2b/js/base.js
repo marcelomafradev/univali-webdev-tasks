2a62f7b3e43f9bbbf5f9a62ea54108aee85a5dbc
marcelomafradev <marcelomafradev@gmail.com>
2026-04-18 20:30:45 -0300

    refactor: remove comments

--- b2b/js/base.js
+++ b2b/js/base.js
@@ -6,7 +6,6 @@ if (toggle && navLinks) {
     navLinks.classList.toggle("open");
   });
 
-  // fecha ao clicar em um link
   navLinks.querySelectorAll("a").forEach((a) => {
     a.addEventListener("click", () => navLinks.classList.remove("open"));
   });
